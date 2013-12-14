6f916eaee3f2e081515facf77dc641384f730c8c
Dan <nixon.daniel.j@gmail.com>
2013-12-13 19:22:48 -0500

    hooked up add to cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  
+  before_destroy :check_referenced_products
   validates :title, :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -10,4 +13,14 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+  
+  private
+    def check_referenced_products
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
