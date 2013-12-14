833a6c39d5bd5d0432d44cf6ae90e8f33eb7f042
Dan <nixon.daniel.j@gmail.com>
2013-12-14 14:54:07 -0500

    smoothed cart actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def tag_hidden_if(tag_name, condition, attrs = {}, &block)
+    if condition
+      attrs["style"] = "display: none;"
+    end
+    content_tag(tag_name, attrs, &block)
+  end
+  
+  def div_hidden_if(condition, attrs = {}, &block)
+    tag_hidden_if("div", condition, attrs, &block)
+  end
 end
